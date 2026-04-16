052e7d4b701371461428496df58cb9b8baa610af
ujeong <qnf323@naver.com>
2026-04-16 22:03:07 +0900

    feat: API 시작시 상영 정보 입력

--- src/main/kotlin/spring/Application.kt
+++ src/main/kotlin/spring/Application.kt
@@ -2,20 +2,6 @@ package spring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
-
-data class Greeting(val id: Long, val name: String) {
-    fun sayHello(): String = "Hello, $name!"
-}
-
-@RestController
-class GreetingController {
-    @GetMapping("/greeting")
-    fun hello(name: String = "AO"): Greeting {
-        return Greeting(1, name)
-    }
-}
 
 @SpringBootApplication(scanBasePackages = ["spring", "repository"])
 class Application
