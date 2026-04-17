c2619e1a2eac8aed981e01a6a939428079b5e95c
ujeong <qnf323@naver.com>
2026-04-17 09:36:46 +0900

    feat: 전체 좌석 반환 추가

--- src/main/kotlin/spring/repository/SeatRepository.kt
+++ src/main/kotlin/spring/repository/SeatRepository.kt
@@ -4,6 +4,7 @@ import domain.seat.Seat
 import domain.seat.SeatCoordinate
 import domain.seat.SeatGrade
 import domain.seat.SeatState
+import domain.seat.Seats
 import javax.sql.DataSource
 import org.springframework.stereotype.Repository
 import view.message.SeatMessages
@@ -24,6 +25,29 @@ class SeatRepository(val dataSource: DataSource) {
                         isReserved = SeatState.AVAILABLE,
                     )
                 }
+            }
+        }
+    }
+
+    fun findAllSeats(): Seats {
+        val sql = "SELECT seat_number, grade FROM seat"
+        return dataSource.connection.use { connection ->
+            connection.prepareStatement(sql).use { ps ->
+                val seats = mutableListOf<Seat>()
+                ps.executeQuery().use { rs ->
+                    while (rs.next()) {
+                        val seatNumber = rs.getString("seat_number")
+
+                        seats.add(
+                            Seat(
+                                coordinate = SeatCoordinate(seatNumber[0], seatNumber.substring(1).toInt()),
+                                grade = SeatGrade.valueOf(rs.getString("grade")),
+                                isReserved = SeatState.AVAILABLE,
+                            ),
+                        )
+                    }
+                }
+                Seats(seats)
             }
         }
     }
